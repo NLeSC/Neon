86ee9eadcfb102b9eb8c253f3c3b0c0603b37c3b
Maarten van Meersbergen <m.vanmeersbergen@esciencecenter.nl>
2013-03-26 09:57:13 +0100

    Fixed warnings.

--- src/nl/esciencecenter/esight/exceptions/InverseNotAvailableException.java
+++ src/nl/esciencecenter/esight/exceptions/InverseNotAvailableException.java
@@ -1,5 +1,6 @@
 package nl.esciencecenter.esight.exceptions;
 
 public class InverseNotAvailableException extends Exception {
+    private static final long serialVersionUID = -2633982121811217072L;
 
 }
--- src/nl/esciencecenter/esight/input/NoSelectionException.java
+++ src/nl/esciencecenter/esight/input/NoSelectionException.java
@@ -1,5 +1,6 @@
 package nl.esciencecenter.esight.input;
 
 public class NoSelectionException extends Exception {
+    private static final long serialVersionUID = 4526969880660161363L;
 
 }
